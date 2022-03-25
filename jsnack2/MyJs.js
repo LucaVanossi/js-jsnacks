05b1f6032daaf47a84a7bc42cb749e68c5d2883e
LucaVanossi <vanossi.luca13@gmail.com>
2022-03-25 18:05:31 +0100

    JSnack2 completato

---
+++ jsnack2/MyJs.js
@@ -0,0 +1,11 @@
+let parola1 = prompt("Please enter your first world");
+
+let parola2 = prompt("Please enter your second world");
+
+if (parola1.length > parola2.lenght) {
+    console.log(parola2, parola1);
+    alert(parola2 + " - " + parola1)
+  } else {
+    console.log(parola1, parola2);
+    alert(parola1 + " - " + parola2)
+  }
